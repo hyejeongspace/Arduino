1b19cf2717704ec7cdbd8611ae25969e1a27a86a
hyejeong <121409233+hyejeongspace@users.noreply.github.com>
2022-12-28 14:43:01 +0900

    Update sketch_project.ino

--- sketch_project.ino
+++ sketch_project.ino
@@ -24,7 +24,6 @@ float duration;
 float distance;
 float distance1;
 int BuzzerPin= 13; // 사용할 포트 핀번호  
-int num = 8; // 몇개의 음을 사용할 것인지  
 int tones[] = {261,293,329,349,391,440,493,523}; // 음계별 주파수  
 
 void _delay(float seconds){
